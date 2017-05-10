5cc6f11ac4291270176a68a32c4f99dc07501ed0
Kien Do <kien.do@liferay.com>
2017-05-10 12:47:00 -0700

    update arandr screen setup

--- arandr/dual_screen_1080p.sh
+++ arandr/dual_screen_1080p.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output HDMI1 --off --output eDP-1-1 --off --output LVDS1 --primary --mode 1920x1080 --pos 0x336 --rotate normal --output VGA1 --off
+xrandr --output DVI-D-1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output VGA-1 --off --output LVDS-1 --primary --mode 1920x1080 --pos 0x240 --rotate normal --output eDP-1-1 --off --output HDMI-1 --off --output DP-1 --off
