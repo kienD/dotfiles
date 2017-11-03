794d368f1fe6783ce91679423a14be078662b322
Kien Do <kien.do@liferay.com>
2017-11-03 16:58:47 -0700

    Make only 1 request on initial login instead of 2

--- scripts/jira_tools.py
+++ scripts/jira_tools.py
@@ -12,22 +12,6 @@ COOKIES_PATH = '/tmp/jira-tools' # look into using the $TMPDIR env variable befo
 REPO_PATH = '/home/kdo/Liferay/ee-plugins'
 SESSION_API_URL = 'https://issues.liferay.com/rest/auth/1/session'
 
-def create_new_session():
-    auth = get_credentials()
-
-    request = requests.get(SESSION_API_URL, headers={'content-type': 'application/json'}, auth=auth)
-
-    print(request.status_code)
-
-    if request.status_code == 401 or request.status_code == 403:
-        print('Try logging in again')
-
-        create_new_session()
-    elif request.status_code == 200:
-        save_cookie(request.cookies['JSESSIONID'])
-    else:
-        print('We cannot complete your request at this time.  Please try again later.')
-
 def get_branch_name():
     repo = pygit2.Repository(REPO_PATH)
 
@@ -40,31 +24,44 @@ def get_credentials():
     return requests.auth.HTTPBasicAuth(username, password)
 
 def get_cookies():
+    print(os.path.exists(COOKIES_PATH))
+    if os.path.exists(COOKIES_PATH):
+        print(os.path.getsize(COOKIES_PATH) > 0)
+
     if os.path.exists(COOKIES_PATH) and os.path.getsize(COOKIES_PATH) > 0:
         file = open(COOKIES_PATH, 'r')
 
         return dict(JSESSIONID=file.read())
-    else:
-        create_new_session()
+
+    return False;
 
 def get_title(branch_name):
-    cookies = get_cookies()
-
     url = 'https://issues.liferay.com/rest/api/2/issue/' + branch_name + '?fields=summary'
 
-    request = requests.get(url, cookies=cookies)
+    cookies = get_cookies()
+
+    if cookies:
+        request = requests.get(url, cookies=cookies)
+    else:
+        auth = get_credentials()
+
+        request = requests.get(url, auth=auth)
+
+        save_cookies(request.cookies['JSESSIONID'])
 
     if request.status_code == 401:
-        create_new_session()
+        os.remove(COOKIES_PATH)
+
+        get_title(branch_name)
     elif request.status_code == 200:
         return branch_name + ' ' + request.json()['fields']['summary']
     else:
         print('We cannot complete your request at this time.  Please try again later.')
 
-def save_cookie(cookie):
+def save_cookies(cookies):
     file = open(COOKIES_PATH, 'w')
 
-    file.write(cookie)
+    file.write(cookies)
 
     file.close()
 
