fb8b8a58fdead8e49d0176be65bc07bf350f0bd7
Kien Do <kien.do@liferay.com>
2021-02-16 14:15:55 -0800

    Create home monitor layout script

---
+++ xorg/home_2_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP2 --off --output DP3 --mode 1920x1080 --pos 0x264 --rotate normal --output VIRTUAL1 --off
