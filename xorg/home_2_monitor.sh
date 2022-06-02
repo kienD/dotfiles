62a38682f406baffdabcbb3e94d113592e964f62
kdo5 <kdo@chegg.com>
2022-06-01 18:15:37 -0700

    Remove unused configuration files

--- xorg/home_2_monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --off --output DP1 --mode 3440x1440 --pos 1920x0 --rotate normal --output DP2 --off --output DP3 --mode 1920x1080 --pos 0x264 --rotate normal --output VIRTUAL1 --off
